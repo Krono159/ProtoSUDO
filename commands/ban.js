3a38a2de5bc4a14263184350c6339fd932c8525e
Luis Felipe Sepulveda <daemo159@gmail.com>
2023-03-01 21:59:13 -0500

    added messageDelete module to Index.js
    Index.js got updated and now it makes an announcement on a pre-specified channel when someone deletes a message. channel can be specified with var "DELCHAN_ID" on config/env file.
    
    Ban module has been re-reactivated after verifying that it's safe to use and it can only be used by trusted users (aka: users with admin permission and/or ban permission)

--- commands/ban.js
+++ commands/ban.js
@@ -14,11 +14,11 @@ module.exports = {
 			option
 				.setName('reason')
 				.setDescription('The reason for banning'))
-		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers || PermissionFlagsBits.Administrator)
 		.setDMPermission(false),
 	async execute(interaction) {
-		/*const target = interaction.options.getUser('target');
 		await interaction.reply('beep beep! this command is under development! any malfunction pls report to <@610937299903184898> or in the development server (https://discord.gg/sB92gTZUGc)');
+		const target = interaction.options.getUser('target');
 		const reason = interaction.options.getString('reason') ?? 'No reason provided';
 
 		await interaction.followUp(`Banning ${target.username} for reason: ${reason}`);
@@ -31,9 +31,7 @@ module.exports = {
 		catch (error) {
 			await interaction.followUp('There was an error! check ban module and related pls.');
 			console.log('There was an error! check ban module and related pls.');
-		}*/
-		await interaction.reply('Command disabled.');
-		console.log('WARNING! COMMAND WAS USED IN PRODUCTION. FEATURE UNSTABLE, PLS FIX.')
+		}
 	},
 
 };
--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-// Require the necessary discord.js classes
+// Require the necessary discord.js classes, possibly this would be required in the future: /* AuditLogEvent*/  at const on line 2
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 const config = require('./config.json');
-
+const { DELCHAN_ID } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
-const { exit } = require('node:process');
+// const { exit } = require('node:process');
+const { EmbedBuilder } = require('discord.js');
 
-console.log('Boop! i just woke up and my token is none of your business, you dumb dork!')
+// initial message
+console.log('yawn! am i alive?');
 
-
-
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
+// instance creation
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 try {
+	// command update & exec module
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		client.commands.set(command.data.name, command);	
+		client.commands.set(command.data.name, command);
 		console.log('Command ', file, ' was loaded successfully');
 	}
-} catch (error) {
-	console.log('please check for on line 20.')
 }
-
+catch (error) {
+	console.log('pls check command update & exec module');
+}
 
 
 client.once(Events.ClientReady, () => {
 	const channelId = config.channel_id;
 	const channel = client.channels.cache.get(channelId);
 	try {
-		channel.send('Hello there, capitan <@610937299903184898>!  I am online!');
-		console.log('welcome message sent!')
-	} catch (error) {
-		console.log('unable to send online message, but bot is alive. pls check index.js client ready block. error: ',error)
+		channel.send('beep!');
+		console.log('welcome message sent!');
 	}
-	console.log('Ready!');
+	catch (error) {
+		console.log('unable to send online message, but bot is alive. pls check index.js client ready block. error: ', error);
+	}
+	console.log('yes i fucking am!');
 });
 
 client.on(Events.InteractionCreate, async interaction => {
@@ -51,13 +52,38 @@ client.on(Events.InteractionCreate, async interaction => {
 
 	try {
 		await command.execute(interaction);
-	} catch (error) {
+	}
+	catch (error) {
 		console.error(error);
 		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.followUp({ content: 'Hubo un error ejecutando este comando!', ephemeral: true });
+		}
+		else {
+			await interaction.reply({ content: 'Hubo un error ejecutando este comando!', ephemeral: true });
 		}
 	}
 });
+
+
+client.on(Events.MessageDelete, async message => {
+	// Ignore direct messages
+	if (!message.guild) return;
+	/*  cont that might be used in future
+		const fetchedLogs = await message.guild.fetchAuditLogs({
+		limit: 1,
+		type: AuditLogEvent.MessageDelete,
+	});*/
+	const channel = client.channels.cache.get(DELCHAN_ID);
+
+	try {
+		const logEmbed = new EmbedBuilder().setColor('#ff0000').setTitle('Message Deleted').setDescription(`un mensaje enviado por ${message.author.name} fue eliminado en ${message.channel}-${message.guild.name}.`).setTimestamp();
+		channel.send({ embeds: [logEmbed] });
+	}
+	catch (error) {
+		console.log(error);
+	}
+
+
+});
+
 client.login(token);
