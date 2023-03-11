e091f349c105b7d75e605b9c9a89141b64032070
Luis Felipe Sepulveda <daemo159@gmail.com>
2023-03-10 21:47:58 -0500

    updated /update command
    added update info.

--- commands/update.js
+++ commands/update.js
@@ -1,12 +1,13 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
-const ver = '0.0.4B hotfix 1';
+const ver = '0.0.4E';
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('update')
 		.setDescription('vamo` a ver que cambios hubieron...')
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
-		const logEmbed = new EmbedBuilder().setColor('#ff55AA').setTitle(`Protosudo V${ver}`).setDescription(`Hey legionarios! ProtoSUDO v${ver} trae algunas novedades! \n se ha actualizado el sistema de reportes. ahora al recibir un reporte, el bot hace ping al equipo administrativo\n\n============================\nV0.0.4B\nSe ha añadido el sistema de reportes. este sistema es algo precario, se recomienda mejorar. Para realizar reportes usar comando "!report" en DM del bot.`);
+		const logEmbed = new EmbedBuilder().setColor('#ff55AA').setTitle(`Protosudo V${ver}`).setDescription(`Hey legionarios! ProtoSUDO **v${ver}** trae algunas novedades! \n trae algunas novedades! \n se ha implementado el comando /bonk y se han añadido 2 imagenes al comando /burunyuu.\n\n============================\n**V0.0.4D**\nse ha actualizado el sistema de reportes. ahora al recibir un reporte, el bot hace ping al equipo administrativo y se ha corregido la sintaxis del modulo principal`);
 		await interaction.reply({ embeds: [logEmbed] });
 	},
 };
+
