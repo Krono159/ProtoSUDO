8225fc8458dc8f2bb89d2f5118aa232b5f8f6944
Luis Felipe Sepulveda <daemo159@gmail.com>
2023-03-18 18:12:42 -0500

    /Burunyuu fix 2
    Command  was keeping the title after getting case 7 as: NYAAAAAAAAAAAA!  {user} mató a necoark D: even in other cases.  cases updated to change the title again to Burunyuu~~.  NFAT

--- commands/burunyuu.js
+++ commands/burunyuu.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
-let title = 'Burunyuu~~';
+let title = '';
 let image = '';
 let color = '';
 
@@ -17,36 +17,42 @@ module.exports = {
 		case 1:
 			image = 'https://media.discordapp.net/attachments/574483864123211791/1080687680637120533/latest.png?width=379&height=676';
 			color = '#FFABCD';
+			title = 'Burunyuu~~';
 			console.log(choice);
 			break;
 
 		case 2:
 			image = 'https://media.discordapp.net/attachments/1079940937310019628/1083894765000605788/neco-neko.gif';
 			color = '#FFFF00';
+			title = 'Burunyuu~~';
 			console.log(choice);
 			break;
 
 		case 3:
 			image = 'https://media.discordapp.net/attachments/574483864123211791/1083812698275463228/TOMORROW.gif';
 			color = '#FF0000';
+			title = 'Burunyuu~~';
 			console.log(choice);
 			break;
 
 		case 4:
 			image = 'https://media.discordapp.net/attachments/1083903422476079246/1085702465279774782/neco_1.gif';
 			color = 'CCAABB';
+			title = 'Burunyuu~~';
             console.log(choice);
 			break;
 
 		case 5:
 			image = 'https://media.discordapp.net/attachments/1083903422476079246/1085701458093146253/neco-arc.gif';
 			color = '#FFABCD';
+			title = 'Burunyuu~~';
             console.log(choice);
 			break;
 
 		case 6:
 			image = 'https://media.discordapp.net/attachments/1083903422476079246/1085703680050868285/neco-arc-cross.gif';
 			color = 'DCBAFF';
+			title = 'Burunyuu~~';
 			console.log(choice);
             break;
 		case 7:
@@ -58,6 +64,7 @@ module.exports = {
 		default:
 			image = 'https://media.discordapp.net/attachments/1083903422476079246/1085703680050868285/neco-arc-cross.gif';
 			color = 'DCBAFF';
+			title = 'Burunyuu~~';
             console.log('default');
 			break;
 		}
